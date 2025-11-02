c7f741ea3fabe35148934ea3512d3d9436934122
dhindalecio <dhindalecio@iesrafaelalberti.es>
2025-11-02 10:37:36 +0100

    Ejercicio 10 resuelto

---
+++ src/Condicionales/ejercicio10.py
@@ -0,0 +1,28 @@
+
+def pedir_pizza():
+    tipo = input("Introduzca si quiere una pizza vegetariana (si/no) :").lower()
+    if tipo == "sí" or tipo == "si":
+        return "Vegetariana"
+    else:
+        return "no vegetariana"
+
+
+def calcular_ingredientes(tipo):
+    if tipo == "Vegetariana":
+        return input("Elige un ingrediente (Pimiento o Tofu): ").lower()
+    
+    else:
+        return input("Elige un ingrediente (Jamón, Salmón, Peperoni): ").lower()
+    
+
+def mostrar_pizza(tipo, ingrediente):
+    print(f"Usted ha elegido una pizza {tipo} y lleva mozarrella, tomate y {ingrediente} ")
+
+
+def main():
+    pedido = pedir_pizza()
+    ingrediente = calcular_ingredientes(pedido)
+    mostrar_pizza(pedido, ingrediente)
+
+if __name__ == "__main__":
+    main()
